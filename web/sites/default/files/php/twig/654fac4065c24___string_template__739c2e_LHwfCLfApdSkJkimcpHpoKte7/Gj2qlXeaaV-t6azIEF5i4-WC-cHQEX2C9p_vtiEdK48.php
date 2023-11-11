b91fdfb23963150cd070890a63103dd4cc5554c4
Paula Rodriguez Romera <prodrig3@ed.ac.uk>
2023-11-11 16:48:02 +0000

    drupal 8 modules practice

---
+++ web/sites/default/files/php/twig/654fac4065c24___string_template__739c2e_LHwfCLfApdSkJkimcpHpoKte7/Gj2qlXeaaV-t6azIEF5i4-WC-cHQEX2C9p_vtiEdK48.php
@@ -0,0 +1,90 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__739c2eca56b0c7f1f8c46c1523248820751da02a2c24ad36e89ef8e17d8bfb3b */
+class __TwigTemplate_662d0e762cf4353e0fa9bac0537ea0a2a9d589dab7a1d3f8c9ce7b35a4335014 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 1, "trans" => 1];
+        $filters = ["escape" => 1];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if', 'trans'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["description"] ?? null)), "html", null, true);
+        if ( !($context["supported"] ?? null)) {
+            echo "<div><small>";
+            echo t("<strong>Not available</strong>. You may need to install external dependencies for use this plugin.", array());
+            echo "</small></div>";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__739c2eca56b0c7f1f8c46c1523248820751da02a2c24ad36e89ef8e17d8bfb3b";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__739c2eca56b0c7f1f8c46c1523248820751da02a2c24ad36e89ef8e17d8bfb3b", "");
+    }
+}
